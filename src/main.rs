c7e1e080c02593e84a6d5f8c1b748341845f2d83
sarendsen <sjoerd@optixdesigns.com>
2021-10-03 17:13:54 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,41 @@ pub const APPLICATION_JSON: &str = "application/json";
 
 #[get("/setprimarydisplay/{id}")]
 pub async fn setprimarydisplay(path: web::Path<String>) -> HttpResponse {
+    // if cfg!(not(target_os = "windows")) {
+    //     return HttpResponse::Ok()
+    //     .content_type(APPLICATION_JSON)
+    //     .json("Only supported on windows")
+    // }
+
     let current_dir = std::env::current_dir().unwrap();
     let display_number = path.0.as_str();
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .current_dir(current_dir)
-            .arg(format!("nircmd.exe setprimarydisplay  {}", display_number))
-            .output()
-            .expect("failed to execute process");
-        String::from("succes")
-    } else {
-        String::from("Only supported on windows")
-    };
+
+    log::info!("current dir: {:?}", current_dir);
+
+    let output = Command::new("cmd")
+        .current_dir(&current_dir)
+        //.arg(format!("./nircmd.exe setprimarydisplay  {}", display_number))
+        //.arg("-c")
+        //.arg("echo hello")
+        //.arg(format!("./test.sh setprimarydisplay  {}", display_number))
+        .args(&["/C", format!("nircmd.exe setprimarydisplay  {}", display_number).as_str()])
+        .output()
+        .expect("failed to execute process");
+
+    match output.status.success() {
+        true => log::info!("stdout: {}", String::from_utf8_lossy(&output.stdout)),
+        false => log::error!("stderr: {}", String::from_utf8_lossy(&output.stderr)),
+    }
 
     HttpResponse::Ok()
         .content_type(APPLICATION_JSON)
-        .json(output)
-    // HttpResponse::NoContent()
-    //     .content_type(APPLICATION_JSON)
-    //     .await
-    //     .unwrap()
+        .json("succes")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    std::env::set_var("RUST_LOG", "info,actix_web=debug,actix_server=info");
     env_logger::init();
-
     HttpServer::new(|| {
         App::new()
             // enable logger - always register actix-web Logger middleware last
@@ -42,7 +50,7 @@ async fn main() -> std::io::Result<()> {
             // register HTTP requests handlers
             .service(setprimarydisplay)
     })
-    .workers(1)
+    .workers(2)
     .bind("0.0.0.0:9090")?
     .run()
     .await
